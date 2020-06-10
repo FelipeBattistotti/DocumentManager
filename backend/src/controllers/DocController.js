46b57e7847f40b70547161341045bcb0413c6363
felipebattistotti <feligatti@gmail.com>
2020-06-10 00:26:33 -0300

    Commit fix

--- backend/src/controllers/DocController.js
+++ backend/src/controllers/DocController.js
@@ -4,11 +4,13 @@ const copyPDF = require('../utils/copyPDF');
 module.exports = {
     async index(request, response) {
         const { page = 1 } = request.query;
+        const user_id = request.headers.authorization;
 
-        const [count] = await connection('doc').count();
+        const [count] = await connection('doc').where('doc.user_id', user_id).count();
 
         const doc = await connection('doc')
             .join('user', 'user.id', '=', 'doc.user_id')
+            .andWhere('doc.user_id', user_id)
             .limit(5)
             .offset((page - 1) * 5)
             .select([
